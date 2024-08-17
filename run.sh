74fadec0ed6ebb24b844376b8fe07f5a837d877d
zzyzzy007 <zzyzzy007@hotmail.com>
2024-08-17 13:02:37 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-!pip install datasets
-import dataset
+import os
+os.system('!pip install datasets')
+import datasets
 raw_dataset = datasets.load_dataset('kde4', lang1='en', lang2='fr', split='train', trust_remote_code=True)
 raw_dataset.save_to_disk('./dataset_kde4')
